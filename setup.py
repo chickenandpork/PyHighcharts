6ca8cff221a6a66bebc2f4ead7cf4bc12d8443cd
Allan Clark <allanc@opensuse11builder.kthxbye>
2014-08-26 23:36:40 -0700

    bump meta for more detail

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from distutils.core import setup
 
+	#https://docs.python.org/2.0/dist/creating-rpms.html
+
 setup(name='PyHighcharts',
 	version='1.0',
 	#py_modules=['foo'],
 	description='A convenient wrapper for Highchart generation procedurally or on the command-line to browser output',
+	long_description='A convenient wrapper for Highchart generation procedurally or on the command-line to browser output.\n\n'+
+	'For documentation on highcharts visit: http://api.highcharts.com/highcharts (Highcharts API)\n\n'+
+	'And remember Highcharts is only free for non-commercial use: Pop over to http://shop.highsoft.com/highcharts.html (Highcharts Licensing) for more info!',
+	license='Free for Non-Commercial use',
 	author='Findlay Yeates',
 	author_email='fidyeates@exeter.ac.uk.unconfirmed',
 	url='https://github.com/fidyeates/PyHighcharts',
